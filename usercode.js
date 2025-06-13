dcb7d96c93d9ac063e99dfe16a46601a7ab174d1
Antonina <62809058+1uckyF0x@users.noreply.github.com>
2025-06-13 18:25:54 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,1 +1,13 @@
 const parser = require('cron-parser');
+
+module.exports = async function(data) {
+    const cronExp = data.cron;
+    try {
+        const interval = parser.parseExpression(cronExp, { currentDate: new Date() });
+        const next = interval.next().toDate();
+        data.nextUnixTime = Math.floor(next.getTime() / 1000);
+    } catch (err) {
+        data.error = err.message;
+    }
+    return data;
+};
